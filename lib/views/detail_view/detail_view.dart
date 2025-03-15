f746edf70cfc545f3b5ae50a6049d5a8e96c4913
Ryunosuke-yo <ryokokawa00@gmail.com>
2025-03-15 22:52:57 +0900

    add debug print

--- lib/views/detail_view/detail_view.dart
+++ lib/views/detail_view/detail_view.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -28,7 +29,9 @@ class DetailView extends HookConsumerWidget {
         final prefs = await SharedPreferences.getInstance();
         final savedList = prefs.getStringList(PrefsKey.favRepoList.name) ?? [];
 
-        print(savedList);
+        if (kDebugMode) {
+          debugPrint('savedList: $savedList');
+        }
 
         final favRepo = json.encode({'owner': owner, 'repo': repo});
 
